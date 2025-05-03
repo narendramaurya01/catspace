b6559e23910a97543e56669cb37385c955a09ef5
narendramaurya01 <134282538+narendramaurya01@users.noreply.github.com>
2025-05-03 18:36:11 +0530

    Update settings.py

--- SOCIAL/settings.py
+++ SOCIAL/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^@j5)b_dqt+wy%ndclcnf4lq+n#d9*5=%w-y5epj)=d&*&o4r&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["blueberry123.pythonanywhere.com","127.0.0.1","localhost","socialpets.up.railway.app"]
 
